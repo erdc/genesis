b7ba9ab8d9173b0023cdf41578a58a999c5cb6df
Kim Pevey <kimberly.c.pevey@erdc.dren.mil>
2019-05-28 15:44:51 -0500

    add test for 2d unstructured

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -1,6 +1,6 @@
 import unittest
 import cartopy.crs as ccrs
-from genesis.mesh import Mesh
+from genesis.mesh import Mesh, Unstruct2D
 
 
 class TestMeshMain(unittest.TestCase):
@@ -12,4 +12,12 @@ class TestMeshMain(unittest.TestCase):
         mesh_object = Mesh(crs=crs)
 
         self.assertIsInstance(mesh_object, Mesh)
+
+    def test_mesh_unstruct2d_instantiation(self):
+        # set crs
+        crs = ccrs.PlateCarree()
+        # instantiate mesh object
+        mesh_object = Unstruct2D(crs=crs)
+
+        self.assertIsInstance(mesh_object, Unstruct2D)
 
