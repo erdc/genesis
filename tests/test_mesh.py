88e59f07deb7c1b3d51a98b52d83fbe25f2123f1
Kim Pevey <kimberly.c.pevey@erdc.dren.mil>
2019-06-05 08:52:52 -0500

    reorg - separate unstructured and 2d

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -1,6 +1,6 @@
 import unittest
 import cartopy.crs as ccrs
-from genesis.mesh import Mesh, Unstruct2D
+from genesis.mesh import Mesh, Unstructured, Unstructured2D
 
 
 class TestMeshMain(unittest.TestCase):
@@ -13,11 +13,18 @@ class TestMeshMain(unittest.TestCase):
 
         self.assertIsInstance(mesh_object, Mesh)
 
+    def test_mesh_unstruct_instantiation(self):
+        # set crs
+        crs = ccrs.PlateCarree()
+        # instantiate mesh object
+        mesh_object = Unstructured(crs=crs)
+
+        self.assertIsInstance(mesh_object, Unstructured)
+
     def test_mesh_unstruct2d_instantiation(self):
         # set crs
         crs = ccrs.PlateCarree()
         # instantiate mesh object
-        mesh_object = Unstruct2D(crs=crs)
+        mesh_object = Unstructured2D(crs=crs)
 
-        self.assertIsInstance(mesh_object, Unstruct2D)
-
+        self.assertIsInstance(mesh_object, Unstructured2D)
