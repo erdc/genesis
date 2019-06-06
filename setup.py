0e064116a953cbe5f6041b2fd221e5d6a9d19f30
Kim Pevey <kimberly.c.pevey@erdc.dren.mil>
2019-06-06 11:10:13 -0500

    Travis update (#8)
    * set default active tools
    
    * reorg - separate unstructured from 2d
    
    * update for new annotators
    
    * update deps
    
    * set up travis

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup, find_packages
 setup(
     name="genesis",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        'param',
+        'panel',
+        'holoviews',
+        'geoviews',
+        'cartopy',
+        'earthsim',
+        'numpy',
+        'pandas',
+        'xarray',
+        'datashader',
+        'colorcet',
+        'jupyter'
+    ],
+    description="GeneSIs (Generic Simulation Interfaces) is a suite of universal base classes for numerical modeling",
+    url="https://github.com/erdc/genesis",
 )
